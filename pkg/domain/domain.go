05b0a09700de1fb3785a6d59e921525e8f2635cc
asstronom <ukraniandollar@gmail.com>
2022-12-16 12:44:51 +0200

    refac: change ordering of methods for UserRepo

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -1,8 +1,17 @@
 package domain
 
+import "context"
+
 type User struct {
 	Id             int32
 	Username       string
 	Password       string
 	ExpirationDays int32 //in days
+}
+
+type UserRepo interface {
+	CreateUser(ctx context.Context, user *User) (int64, error)
+	GetUserById(ctx context.Context, id int64) (*User, error)
+	UpdateUser(ctx context.Context, user *User) error
+	DeleteUser(ctx context.Context, id int64) error
 }
