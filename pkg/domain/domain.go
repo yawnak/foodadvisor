8a0689bfa98b7136a1908e2068ba4972975afd4d
asstronom <ukraniandollar@gmail.com>
2022-12-17 21:49:58 +0200

    feat: add Food and Ingridient

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -1,6 +1,9 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type User struct {
 	Id             int32
@@ -14,4 +17,22 @@ type UserRepo interface {
 	GetUserById(ctx context.Context, id int64) (*User, error)
 	UpdateUser(ctx context.Context, user *User) error
 	DeleteUser(ctx context.Context, id int64) error
+}
+
+type Ingridient struct {
+	Id   int32
+	Name string
+}
+
+type Food struct {
+	Id          int32
+	Name        string
+	CookTime    time.Time
+	Price       int32
+	IsBreakfast bool
+	IsDinner    bool
+	IsSupper    bool
+}
+
+type FoodRepo interface {
 }
