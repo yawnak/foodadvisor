6957766b1002d07f69e73d7bcf399fa20f38afd4
asstronom <ukraniandollar@gmail.com>
2022-12-23 17:44:29 +0200

    feat: add CRUD for food

--- pkg/db/food.go
+++ pkg/db/food.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/asstronom/foodadvisor/pkg/domain"
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/jackc/pgx/pgtype"
@@ -12,13 +15,13 @@ var (
 )
 
 type food struct {
-	Id          pgtype.Int4
-	Name        pgtype.Varchar
-	CookTime    pgtype.Interval
-	Price       pgtype.Int4
-	IsBreakfast pgtype.Bool
-	IsDinner    pgtype.Bool
-	IsSupper    pgtype.Bool
+	Id          pgtype.Int4     `db:"id"`
+	Name        pgtype.Varchar  `db:"name" fieldtag:"details"`
+	CookTime    pgtype.Interval `db:"cooktime" fieldtag:"details"`
+	Price       pgtype.Int4     `db:"price" fieldtag:"details"`
+	IsBreakfast pgtype.Bool     `db:"isbreakfast" fieldtag:"details"`
+	IsDinner    pgtype.Bool     `db:"isdinner" fieldtag:"details"`
+	IsSupper    pgtype.Bool     `db:"issupper" fieldtag:"details"`
 }
 
 func foodToFoodRepo(f *domain.Food) *food {
@@ -33,7 +36,7 @@ func foodToFoodRepo(f *domain.Food) *food {
 	return &res
 }
 
-func foodRepoToFood(f food) *domain.Food {
+func foodRepoToFood(f *food) *domain.Food {
 	return &domain.Food{
 		Id:          f.Id.Int,
 		Name:        f.Name.String,
@@ -43,4 +46,55 @@ func foodRepoToFood(f food) *domain.Food {
 		IsDinner:    f.IsDinner.Bool,
 		IsSupper:    f.IsSupper.Bool,
 	}
+}
+
+func (db *FoodDB) GetFoodById(ctx context.Context, id int32) (*domain.Food, error) {
+	sb := foodStruct.SelectFrom(foodTable)
+	sb.Where(sb.Equal("id", id))
+	sql, args := sb.BuildWithFlavor(sqlbuilder.PostgreSQL)
+
+	var food food
+	row := db.pool.QueryRow(ctx, sql, args...)
+	err := row.Scan(userStruct.Addr(&food)...)
+	if err != nil {
+		return nil, fmt.Errorf("error scanning user: %w", err)
+	}
+	return foodRepoToFood(&food), nil
+}
+
+func (db *FoodDB) CreateFood(ctx context.Context, food *domain.Food) (int32, error) {
+	f := foodToFoodRepo(food)
+	sb := foodStruct.InsertIntoForTag(foodTable, "details", userStruct.ValuesForTag("details", f)...)
+	sql, args := sb.BuildWithFlavor(sqlbuilder.PostgreSQL)
+	sql += " RETURNING id"
+	row := db.pool.QueryRow(ctx, sql, args...)
+	var id int32
+	err := row.Scan(&id)
+	if err != nil {
+		return 0, fmt.Errorf("error scanning returning id: %w", err)
+	}
+	return id, nil
+}
+
+func (db *FoodDB) DeleteFood(ctx context.Context, id int32) error {
+	sb := foodStruct.DeleteFrom(foodTable)
+	sb.Where(sb.Equal("id", id))
+	sql, args := sb.BuildWithFlavor(sqlbuilder.PostgreSQL)
+	_, err := db.pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("error executing query: %w", err)
+	}
+	return nil
+}
+
+func (db *FoodDB) UpdateFood(ctx context.Context, food *domain.Food) error {
+	foodRepo := foodToFoodRepo(food)
+	sb := userStruct.UpdateForTag(usersTable, "details", userStruct.ValuesForTag("details", &foodRepo))
+	sb.Where(sb.Equal("id", foodRepo.Id))
+	sql, args := sb.BuildWithFlavor(sqlbuilder.PostgreSQL)
+	_, err := db.pool.Exec(ctx, sql, args...)
+	if err != nil {
+		return fmt.Errorf("error executring query: %w", err)
+	}
+	return err
 }
