5945830fdfe9710c3838a883a8f089fe8b156c4a
asstronom <ukraniandollar@gmail.com>
2022-12-23 17:35:36 +0200

    feat: conversions

--- pkg/db/food.go
+++ pkg/db/food.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/asstronom/foodadvisor/pkg/domain"
 	"github.com/huandu/go-sqlbuilder"
 	"github.com/jackc/pgx/pgtype"
 )
@@ -18,4 +19,28 @@ type food struct {
 	IsBreakfast pgtype.Bool
 	IsDinner    pgtype.Bool
 	IsSupper    pgtype.Bool
+}
+
+func foodToFoodRepo(f *domain.Food) *food {
+	res := food{}
+	res.Id.Set(f.Id)
+	res.Name.Set(f.Name)
+	res.CookTime.Set(f.CookTime)
+	res.Price.Set(f.Price)
+	res.IsBreakfast.Set(f.IsBreakfast)
+	res.IsDinner.Set(f.IsDinner)
+	res.IsSupper.Set(f.IsSupper)
+	return &res
+}
+
+func foodRepoToFood(f food) *domain.Food {
+	return &domain.Food{
+		Id:          f.Id.Int,
+		Name:        f.Name.String,
+		CookTime:    int32(f.CookTime.Microseconds / 1000),
+		Price:       f.Price.Int,
+		IsBreakfast: f.IsBreakfast.Bool,
+		IsDinner:    f.IsDinner.Bool,
+		IsSupper:    f.IsSupper.Bool,
+	}
 }
