9edc1a5aaeb29d9bd630be716011786a0c2e8105
asstronom <ukraniandollar@gmail.com>
2022-12-23 17:28:13 +0200

    feat: add food struct with pgtype

--- pkg/db/food.go
+++ pkg/db/food.go
@@ -1,6 +1,9 @@
 package db
 
-import "github.com/huandu/go-sqlbuilder"
+import (
+	"github.com/huandu/go-sqlbuilder"
+	"github.com/jackc/pgx/pgtype"
+)
 
 var (
 	foodStruct = sqlbuilder.NewStruct(new(food))
@@ -8,4 +11,11 @@ var (
 )
 
 type food struct {
+	Id          pgtype.Int4
+	Name        pgtype.Varchar
+	CookTime    pgtype.Interval
+	Price       pgtype.Int4
+	IsBreakfast pgtype.Bool
+	IsDinner    pgtype.Bool
+	IsSupper    pgtype.Bool
 }
