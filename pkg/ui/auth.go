d1ddcadff6f05a9fc66ecfcfa8ff5b428bac187b
asstronom <ukraniandollar@gmail.com>
2022-12-24 10:52:27 +0200

    refactor

--- pkg/ui/auth.go
+++ pkg/ui/auth.go
@@ -1,10 +1,52 @@
 package ui
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 
+	"github.com/asstronom/foodadvisor/pkg/domain"
 	"github.com/manifoldco/promptui"
 )
+
+func (cli *UICli) CredentialsPrompt() (string, string, error) {
+	prompt := promptui.Prompt{
+		Label: "Enter username:",
+	}
+	username, err := prompt.Run()
+	if err != nil {
+		return "", "", fmt.Errorf("error getting username: %w", err)
+	}
+	prompt = promptui.Prompt{
+		Label: "Enter password",
+		Mask:  '*',
+	}
+	password, err := prompt.Run()
+	if err != nil {
+		return "", "", fmt.Errorf("error getting password: %w", err)
+	}
+	return username, password, nil
+}
+
+func (cli *UICli) ExpirationPrompt() (int32, error) {
+	validate := func(input string) error {
+		_, err := strconv.Atoi(input)
+		if err != nil {
+			return errors.New("not a number")
+		}
+		return nil
+	}
+	prompt := promptui.Prompt{
+		Label:    "Enter expiration days",
+		Validate: validate,
+	}
+	s, err := prompt.Run()
+	if err != nil {
+		return 0, err
+	}
+	n, _ := strconv.Atoi(s)
+	return int32(n), nil
+}
 
 func (cli *UICli) AuthenticationMenu() error {
 	prompt := promptui.Select{
@@ -36,59 +78,30 @@ func (cli *UICli) AuthenticationMenu() error {
 }
 
 func (cli *UICli) LoginPrompt() error {
-	prompt := promptui.Prompt{
-		Label: "Enter username:",
-	}
-	username, err := prompt.Run()
+	username, password, err := cli.CredentialsPrompt()
 	if err != nil {
-		return fmt.Errorf("error getting username: %w", err)
-	}
-	prompt = promptui.Prompt{
-		Label: "Enter password",
-		Mask:  '*',
-	}
-	password, err := prompt.Run()
-	if err != nil {
-		return fmt.Errorf("error getting password: %w", err)
+		return fmt.Errorf("error getting credentials: %w", err)
 	}
 	fmt.Printf("username: %s, password: %s\n", username, password)
 	return nil
 }
 
 func (cli *UICli) RegisterPrompt() error {
-	prompt := promptui.Prompt{
-		Label: "Enter username:",
-	}
-	username, err := prompt.Run()
+	var user domain.User
+	var err error
+
+	user.Username, user.Password, err = cli.CredentialsPrompt()
+
 	if err != nil {
-		return fmt.Errorf("error getting username: %w", err)
-	}
-	prompt = promptui.Prompt{
-		Label: "Enter password",
-		Mask:  '*',
-	}
-	password, err := prompt.Run()
-	if err != nil {
-		return fmt.Errorf("error getting password: %w", err)
+		return fmt.Errorf("error getting credentials: %w", err)
 	}
 
-	prompt = promptui.Prompt{
-		Label: "Confirm password",
-		Mask:  '*',
-	}
-	confirmPassword, err := prompt.Run()
+	user.ExpirationDays, err = cli.ExpirationPrompt()
+
 	if err != nil {
-		return fmt.Errorf("error getting password: %w", err)
+		return fmt.Errorf("error getting expiration: %w", err)
 	}
 
-	if confirmPassword != password {
-		fmt.Println("passwords don't match, try again")
-		err = cli.RegisterPrompt()
-		if err != nil {
-			return fmt.Errorf("error retrying: %w", err)
-		}
-	} else {
-		fmt.Printf("username: %s, password: %s\n", username, password)
-	}
+	fmt.Println(user)
 	return nil
 }
