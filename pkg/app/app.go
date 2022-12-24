de9d65e8fb6cb82db313c7df05f226e631f48cde
asstronom <ukraniandollar@gmail.com>
2022-12-24 11:08:11 +0200

    feat: add constructor for food advisor

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -10,6 +10,12 @@ type FoodAdvisor struct {
 	db domain.AdvisorRepo
 }
 
+func NewFoodAdvisor(repo domain.AdvisorRepo) (*FoodAdvisor, error) {
+	var adv *FoodAdvisor
+	adv.db = repo
+	return adv, nil
+}
+
 func (adv *FoodAdvisor) CreateUser(ctx context.Context, user *domain.User) (int32, error) {
 	id, err := adv.db.CreateUser(ctx, user)
 	return id, err
