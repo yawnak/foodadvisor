10e399e340cab0081c6e415a6308864e8179dcd9
asstronom <ukraniandollar@gmail.com>
2022-12-24 13:07:07 +0200

    new migrations

---
+++ pkg/db/migrations/sql/000002_create_enums.up.sql
@@ -0,0 +1,3 @@
+CREATE TYPE meal AS ENUM ('breakfast', 'dinner', 'supper')
+
+CREATE TYPE dish AS ENUM ('soup', 'porridge', 'puree', 'desert', 'cake', 'cutlet', 'dumpling')
--- pkg/db/migrations/sql/000002_create_food.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE foodtouser;
-
-DROP TABLE food;
