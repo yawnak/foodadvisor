308cd63dccd835ebcd199fc543490d3f576d004c
asstronom <ukraniandollar@gmail.com>
2022-12-24 11:24:27 +0200

    feat: add GettingUserByCredentials

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,8 @@ package app
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/asstronom/foodadvisor/pkg/domain"
 )
@@ -22,5 +24,19 @@ func NewFoodAdvisor(repo domain.AdvisorRepo) (*FoodAdvisor, error) {
 
 func (adv *FoodAdvisor) CreateUser(ctx context.Context, user *domain.User) (int32, error) {
 	id, err := adv.db.CreateUser(ctx, user)
-	return id, err
+	if err != nil {
+		return 0, fmt.Errorf("error creating user: %w", err)
+	}
+	return id, nil
+}
+
+func (adv *FoodAdvisor) GetUserByCredentials(ctx context.Context, username string, password string) (*domain.User, error) {
+	user, err := adv.db.GetUserByUsername(ctx, username)
+	if err != nil {
+		return nil, fmt.Errorf("error getting user by username: %w", err)
+	}
+	if user.Username != username || user.Password != password {
+		return nil, ErrWrongCredentials
+	}
+	return user, nil
 }
