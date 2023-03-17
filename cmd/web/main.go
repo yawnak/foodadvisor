acc38cd803455c2b2bd928b95732bce3c06603b2
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-17 23:40:22 +0200

    do stuff with configs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
-	"github.com/asstronom/foodadvisor/cmd/web/config"
 	"github.com/asstronom/foodadvisor/pkg/app"
+	"github.com/asstronom/foodadvisor/pkg/config"
 	"github.com/asstronom/foodadvisor/pkg/db"
 )
 
+var (
+	isDev bool
+)
+
+func parseFlags() {
+	flag.BoolVar(&isDev, "dev", false, "set development mode")
+	flag.Parse()
+}
+
 func main() {
-	dbconf, err := config.ParseDBConnConfig(`C:\Users\danya\Documents\foodadvisor\configs\dbconf.yaml`)
+	parseFlags()
+
+	dbconf, err := config.ParseDBConnConfig(`configs\dbconf.yaml`)
 	if err != nil {
 		log.Fatalf("error parsing db conf: %s\n", err)
 	}
 
+	if isDev {
+		dbconf, err = config.ParseDBConnConfig(`configs\dbconf_dev.yaml`)
+		if err != nil {
+			log.Fatalf("error parsing dev conf: %s\n", err)
+		}
+	}
+	fmt.Println(dbconf)
+	log.Fatal()
 	dburl := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		dbconf.User, dbconf.Password, dbconf.Host, dbconf.Port, dbconf.Name)
 	fmt.Println(dburl)
