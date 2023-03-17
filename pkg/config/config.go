59e1be9bb5ba612089341c0ea855acea8a9b212b
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-17 23:56:17 +0200

    fix struct tags and remove passwords from configs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,21 +1,19 @@
 package config
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"path/filepath"
 
-	"github.com/sethvargo/go-envconfig"
 	"gopkg.in/yaml.v3"
 )
 
 type DBConnConfig struct {
-	Host     string `env:"HOST,required" yaml:"dbhost"`
-	Port     string `env:"PORT,required" yaml:"dbport"`
-	User     string `env:"USER,required" yaml:"dbuser"`
-	Password string `env:"PASSWORD,required" yaml:"dbpassword"`
-	Name     string `env:"NAME,required" yaml:"dbname"`
+	Host     string `yaml:"dbhost"`
+	Port     string `yaml:"dbport"`
+	User     string `yaml:"dbuser"`
+	Name     string `yaml:"dbname"`
+	Password string
 }
 
 func ParseDBConnConfig(path string) (*DBConnConfig, error) {
@@ -36,12 +34,12 @@ func ParseDBConnConfig(path string) (*DBConnConfig, error) {
 	return &dbconf, nil
 }
 
-func ParseDBConnConfigEnv(ctx context.Context, prefix string) (*DBConnConfig, error) {
-	var dbconf DBConnConfig
-	l := envconfig.PrefixLookuper(prefix, envconfig.OsLookuper())
-	err := envconfig.ProcessWith(ctx, &dbconf, l)
-	if err != nil {
-		return nil, fmt.Errorf("error processing: %w", err)
-	}
-	return &dbconf, err
-}
+// func ParseDBConnConfigEnv(ctx context.Context, prefix string) (*DBConnConfig, error) {
+// 	var dbconf DBConnConfig
+// 	l := envconfig.PrefixLookuper(prefix, envconfig.OsLookuper())
+// 	err := envconfig.ProcessWith(ctx, &dbconf, l)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error processing: %w", err)
+// 	}
+// 	return &dbconf, err
+// }
