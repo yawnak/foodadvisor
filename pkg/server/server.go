46361adb60eeb92766f811cc13e80a5ce25d7367
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 16:33:47 +0200

    feat: add server

---
+++ pkg/server/server.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"github.com/asstronom/foodadvisor/pkg/domain"
+	"github.com/gorilla/mux"
+)
+
+type Server struct {
+	app    domain.Advisor
+	router *mux.Router
+}
+
+func NewServer(app domain.Advisor) (*Server, error) {
+	srv := Server{
+		app:    app,
+		router: mux.NewRouter(),
+	}
+
+	srv.initRoutes()
+
+	return &srv, nil
+}
