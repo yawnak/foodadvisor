34d02bd29a27dd90b5a2ddb2eb3406f47629fc3d
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 17:00:28 +0200

    feat: add middleware for checking content-type

---
+++ pkg/server/middleware.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/golang/gddo/httputil/header"
+)
+
+// validates that request content type is set to JSON
+func (srv *Server) validateContentJSON(http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Type") != "" {
+			value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+			if value != "application/json" {
+				writeErrorAsJSON(w, http.StatusUnsupportedMediaType, ErrRequiredContentTypeJSON)
+			}
+		}
+	}
+}
