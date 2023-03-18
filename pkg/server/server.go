7875cfa02bb15ea6333064be5c700c29cd736e50
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 17:11:49 +0200

    feat: add listen and serve for server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/asstronom/foodadvisor/pkg/domain"
 	"github.com/gorilla/mux"
 )
@@ -19,4 +21,8 @@ func NewServer(app domain.Advisor) (*Server, error) {
 	srv.initAPIRoutes()
 
 	return &srv, nil
+}
+
+func (srv *Server) ListenAndServe(port string) error {
+	return http.ListenAndServe(":"+port, srv.router)
 }
