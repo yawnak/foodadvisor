53da58d6e33ee8a17a36e69f9748baf273c1660b
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 18:21:27 +0200

    fix: error when getting empty content type header

--- pkg/server/middleware.go
+++ pkg/server/middleware.go
@@ -1,20 +1,23 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/golang/gddo/httputil/header"
 )
 
+
+
 // validates that request content type is set to JSON
 func validateContentJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Content-Type") != "" {
-			value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
-			if value != "application/json" {
-				writeErrorAsJSON(w, http.StatusUnsupportedMediaType, ErrRequiredContentTypeJSON)
-				return
-			}
+		fmt.Println("validating content-type")
+		fmt.Println("content type: ", r.Header.Get("Content-Type"))
+		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
+		if value != "application/json" {
+			writeErrorAsJSON(w, http.StatusUnsupportedMediaType, ErrRequiredContentTypeJSON)
+			return
 		}
 		next.ServeHTTP(w, r)
 	}
