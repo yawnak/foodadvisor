4c33b2851b9615fc786a280d111bdddeb8a87664
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 22:13:18 +0200

    feat: add token generation

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,15 +2,22 @@ package app
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"time"
 
 	"github.com/asstronom/foodadvisor/pkg/domain"
+	"github.com/golang-jwt/jwt"
 )
 
-var (
-	ErrWrongCredentials = errors.New("wrong credentials")
+const (
+	signingKey = "pudgebooster"
+	tokenTTL   = 12 * time.Hour
 )
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserID int32 `json:"user_id"`
+}
 
 type FoodAdvisor struct {
 	db domain.AdvisorRepo
@@ -20,6 +27,24 @@ func NewFoodAdvisor(repo domain.AdvisorRepo) (*FoodAdvisor, error) {
 	var adv FoodAdvisor
 	adv.db = repo
 	return &adv, nil
+}
+
+func (c *FoodAdvisor) GenerateToken(ctx context.Context, username string, password string) (string, error) {
+	user, err := c.db.GetUserByUsername(ctx, username)
+	if err != nil {
+		return "", fmt.Errorf("error getting user: %w", err)
+	}
+	if user.Username != username || user.Password != password {
+		return "", domain.ErrWrongCredentials
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+	return token.SignedString([]byte(signingKey))
 }
 
 func (adv *FoodAdvisor) CreateUser(ctx context.Context, user *domain.User) (int32, error) {
@@ -36,7 +61,7 @@ func (adv *FoodAdvisor) GetUserByCredentials(ctx context.Context, username strin
 		return nil, fmt.Errorf("error getting user by username: %w", err)
 	}
 	if user.Username != username || user.Password != password {
-		return nil, ErrWrongCredentials
+		return nil, domain.ErrWrongCredentials
 	}
 	return user, nil
 }
