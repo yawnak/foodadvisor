e39445c54c6b826f3fb235af3b5ab5e9f940ad4e
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 18:22:41 +0200

    rev: prints

--- pkg/server/middleware.go
+++ pkg/server/middleware.go
@@ -12,8 +12,6 @@ import (
 // validates that request content type is set to JSON
 func validateContentJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("validating content-type")
-		fmt.Println("content type: ", r.Header.Get("Content-Type"))
 		value, _ := header.ParseValueAndParams(r.Header, "Content-Type")
 		if value != "application/json" {
 			writeErrorAsJSON(w, http.StatusUnsupportedMediaType, ErrRequiredContentTypeJSON)
