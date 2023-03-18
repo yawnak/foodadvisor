fc352e21e2aa8542afab4d327746c8902841d533
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 17:07:05 +0200

    feat: add signup and login funcs

---
+++ pkg/server/handlers.go
@@ -0,0 +1,11 @@
+package server
+
+import "net/http"
+
+func (srv *Server) signup(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func (srv *Server) login(w http.ResponseWriter, r *http.Request) {
+
+}
