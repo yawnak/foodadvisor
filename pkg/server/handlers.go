fddfebbab7569482676a1a3ff28cae0e70a4261a
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-18 20:44:31 +0200

    feat: add binding of request to domain.User

--- pkg/server/handlers.go
+++ pkg/server/handlers.go
@@ -1,8 +1,56 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/asstronom/foodadvisor/pkg/domain"
+)
 
 func (srv *Server) signup(w http.ResponseWriter, r *http.Request) {
+	var err error
+	//unmarshaling request to domain.User
+	r.Body = http.MaxBytesReader(w, r.Body, 1<<20) //set maximum size of request body to 1mb
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields() //will not allow sending unknown fields in request
+	var user domain.User
+	err = dec.Decode(&user)
+	//processing errors
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		switch {
+		case errors.As(err, &syntaxError):
+			err = fmt.Errorf("request body contains bad JSON (at position %d)", syntaxError.Offset)
+			writeErrorAsJSON(w, http.StatusBadRequest, err)
+			return
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			err = errors.New("request body contains badly-formed JSON")
+			writeErrorAsJSON(w, http.StatusBadRequest, err)
+			return
+		case errors.As(err, &unmarshalTypeError):
+			err = fmt.Errorf("request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			writeErrorAsJSON(w, http.StatusBadRequest, err)
+			return
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			err = fmt.Errorf("request body contains unknown field %s", fieldName)
+			writeErrorAsJSON(w, http.StatusBadRequest, err)
+			return
+		case errors.Is(err, io.EOF):
+			writeErrorAsJSON(w, http.StatusBadRequest, ErrEmptyRequestBody)
+			return
+		default:
+			log.Print(err.Error())
+			writeErrorAsJSON(w, http.StatusInternalServerError, ErrInternal)
+		}
+	}
+
 	
 }
 
