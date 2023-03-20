63d547860af467ba95c0b4dff45b3342eec8b6c5
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-20 16:24:33 +0200

    feat: add  auth middleware

--- pkg/server/middleware.go
+++ pkg/server/middleware.go
@@ -1,9 +1,13 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/golang/gddo/httputil/header"
+	"github.com/yawnak/foodadvisor/pkg/domain"
 )
 
 // validates that request content type is set to JSON
@@ -15,5 +19,34 @@ func validateContentJSON(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 		next.ServeHTTP(w, r)
+	}
+}
+
+func (srv *Server) auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie(authCookieName)
+		if err != nil {
+			switch {
+			case errors.Is(err, http.ErrNoCookie):
+				writeErrorAsJSON(w, http.StatusUnauthorized, errors.New("cookie with auth token is not present"))
+				return
+			}
+		}
+		id, err := srv.app.ParseToken(r.Context(), cookie.Value)
+		if err != nil {
+			switch {
+			case errors.Is(err, domain.ErrBadToken):
+				writeErrorAsJSON(w, http.StatusUnauthorized, err)
+				return
+			case errors.Is(err, domain.ErrInvalidSigningMethod):
+				writeErrorAsJSON(w, http.StatusUnauthorized, err)
+				return
+			default:
+				log.Println(err)
+				writeErrorAsJSON(w, http.StatusInternalServerError, errors.New("error validating auth token cookie"))
+				return
+			}
+		}
+		next(w, r.WithContext(context.WithValue(r.Context(), keyUserId, id)))
 	}
 }
