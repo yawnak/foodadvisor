6d0512e6fbcc8fea7e0414d01dc277f883ce427a
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-23 16:59:54 +0200

    feat: add ErrDuplicateResourse and ErrNotFound

--- pkg/domain/errors.go
+++ pkg/domain/errors.go
@@ -8,4 +8,6 @@ var (
 	ErrPasswordTooLong      = errors.New("password too long")
 	ErrBadToken             = errors.New("auth token is of wrong format")
 	ErrInvalidSigningMethod = errors.New("token is signed with wrong method")
+	ErrDuplicateResourse    = errors.New("resourse already exists")
+	ErrResourseNotFound     = errors.New("resourse not found")
 )
