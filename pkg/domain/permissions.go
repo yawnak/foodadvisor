82f8915c62d378eaa983c9cd4b758afcd9bf0a1c
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-24 19:14:33 +0200

    feat: add constructor for Role

--- pkg/domain/permissions.go
+++ pkg/domain/permissions.go
@@ -11,3 +11,14 @@ type Role struct {
 	Name        string
 	Permissions map[Permission]struct{}
 }
+
+func NewRole(name string, permissions ...Permission) *Role {
+	role := Role{
+		Name:        name,
+		Permissions: make(map[Permission]struct{}),
+	}
+	for _, p := range permissions {
+		role.Permissions[p] = struct{}{}
+	}
+	return &role
+}
