b491ed2a145c10f1991f993d45cefdb5856db154
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-26 19:02:28 +0300

    test: middleware authenticate + confirmPermissions

--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/yawnak/foodadvisor/pkg/domain"
 )
 
 func (srv *Server) initAPIRoutes() {
 	api := srv.router.PathPrefix("/api").Subrouter()
-
 	api.HandleFunc("/signup", validateContentJSON(srv.signup)).Methods("POST")
 	api.HandleFunc("/login", validateContentJSON(srv.login)).Methods("POST")
 
-	api.HandleFunc("/profile", srv.auth(func(w http.ResponseWriter, r *http.Request) {
+	api.HandleFunc("/profile", srv.authTokenToContext(func(w http.ResponseWriter, r *http.Request) {
 		id, ok := retrieveUserId(r.Context())
 		if !ok {
 			writeErrorAsJSON(w, http.StatusInternalServerError, errors.New("ERROR: not authorized"))
@@ -22,4 +23,18 @@ func (srv *Server) initAPIRoutes() {
 		}
 		fmt.Fprintf(w, "hello user number: %d", id)
 	})).Methods("GET")
+
+	roles := api.PathPrefix("/roles").Subrouter()
+	roles.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		role, ok := retrieveRole(r.Context())
+		log.Println("ROLES")
+		if !ok {
+			writeErrorAsJSON(w, http.StatusInternalServerError, errors.New("not authorized"))
+			log.Fatalln("didn't work. no role in context")
+			return
+		}
+		fmt.Fprintf(w, "hello user with role: %s", role.Name)
+	}).Methods("GET")
+	roles.Use(srv.authenticate)
+	roles.Use(confirmPermissions(domain.PermEditRoles))
 }
