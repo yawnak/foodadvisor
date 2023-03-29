76fa357aea5f3b66622f1a73a7ca7f73ffd590d6
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-30 00:41:01 +0300

    feat: bindingErrorToHTTPStatus

--- pkg/server/middleware.go
+++ pkg/server/middleware.go
@@ -2,11 +2,14 @@ package server
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/golang/gddo/httputil/header"
 	"github.com/gorilla/mux"
+	"github.com/xorcare/pointer"
+	"github.com/yawnak/foodadvisor/pkg/bind"
 	"github.com/yawnak/foodadvisor/pkg/domain"
 )
 
@@ -58,3 +61,30 @@ func confirmPermissions(permissions ...domain.Permission) mux.MiddlewareFunc {
 		})
 	}
 }
+
+// return http.StatusOK if err = nil
+// return http.StatusInternalServerError if error is unknown
+// other - if error is known (mostly http.StatusBadRequest)
+func bindingErrorToHTTPStatus(err error) int {
+	if err != nil {
+		switch {
+		case errors.Is(err, bind.ErrBadFormat):
+			return http.StatusBadRequest
+		case errors.Is(err, bind.ErrBodyTooLarge):
+			return http.StatusBadRequest
+		case errors.Is(err, bind.ErrEmptyBody):
+			return http.StatusBadRequest
+		case errors.As(err, pointer.Of(&bind.ErrSyntax{})):
+			return http.StatusBadRequest
+		case errors.As(err, pointer.Of(&bind.ErrUnmarshalType{})):
+			return http.StatusBadRequest
+		case errors.As(err, pointer.Of(&bind.ErrUnknownField{})):
+			return http.StatusBadRequest
+		case errors.As(err, pointer.Of(&bind.ErrUnknown{})):
+			return http.StatusInternalServerError
+		}
+	}
+	return http.StatusOK
+}
+
+
