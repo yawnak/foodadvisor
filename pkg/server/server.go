b7fa702451786cfd957c6ecba2dfa1c09f0ef758
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-30 00:41:35 +0300

    feat: add validator to server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,14 +2,19 @@ package server
 
 import (
 	"net/http"
+	"reflect"
+	"regexp"
+	"strings"
 
-	"github.com/yawnak/foodadvisor/pkg/domain"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
+	"github.com/yawnak/foodadvisor/pkg/domain"
 )
 
 type Server struct {
-	app    domain.Advisor
-	router *mux.Router
+	app      domain.Advisor
+	router   *mux.Router
+	validate *validator.Validate
 }
 
 func NewServer(app domain.Advisor) (*Server, error) {
@@ -19,8 +24,30 @@ func NewServer(app domain.Advisor) (*Server, error) {
 	}
 
 	srv.initAPIRoutes()
-
+	srv.initValidator()
 	return &srv, nil
+}
+
+func (srv *Server) initValidator() error {
+	srv.validate = validator.New()
+	srv.validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		// skip if tag key says it should be ignored
+		if name == "-" {
+			return ""
+		}
+		return name
+	})
+
+	srv.validate.RegisterAlias("usernamemax", "lte=30")
+	srv.validate.RegisterAlias("passwordmax", "lte=72")
+
+	srv.validate.RegisterValidation("alphanumwithunderscore", func(fl validator.FieldLevel) bool {
+		val := fl.Field().String()
+		rg := regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+		return rg.MatchString(val)
+	})
+	return nil
 }
 
 func (srv *Server) ListenAndServe(port string) error {
