39d8d74e015416a7df6d00ecc6b74a202b07be09
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-30 00:42:10 +0300

    refac: parse json using bind package

--- pkg/server/auth.go
+++ pkg/server/auth.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/yawnak/foodadvisor/pkg/bind"
 	"github.com/yawnak/foodadvisor/pkg/domain"
 )
 
@@ -20,42 +21,25 @@ const (
 
 func (srv *Server) signup(w http.ResponseWriter, r *http.Request) {
 	var err error
-	//unmarshaling request to domain.User
-	r.Body = http.MaxBytesReader(w, r.Body, 1<<20) //set maximum size of request body to 1mb
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields() //will not allow sending unknown fields in request
 	var user domain.User
-	err = dec.Decode(&user)
-	//processing errors
-	if err != nil {
-		var syntaxError *json.SyntaxError
-		var unmarshalTypeError *json.UnmarshalTypeError
-		switch {
-		case errors.As(err, &syntaxError):
-			err = fmt.Errorf("request body contains bad JSON (at position %d)", syntaxError.Offset)
-			writeErrorAsJSON(w, http.StatusBadRequest, err)
-			return
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			err = errors.New("request body contains badly-formed JSON")
-			writeErrorAsJSON(w, http.StatusBadRequest, err)
-			return
-		case errors.As(err, &unmarshalTypeError):
-			err = fmt.Errorf("request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
-			writeErrorAsJSON(w, http.StatusBadRequest, err)
-			return
-		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
-			err = fmt.Errorf("request body contains unknown field %s", fieldName)
-			writeErrorAsJSON(w, http.StatusBadRequest, err)
-			return
-		case errors.Is(err, io.EOF):
-			writeErrorAsJSON(w, http.StatusBadRequest, ErrEmptyRequestBody)
-			return
-		default:
-			log.Print(err.Error())
-			writeErrorAsJSON(w, http.StatusInternalServerError, ErrInternal)
-			return
-		}
+	//unmarshaling request to domain.User
+	binder := bind.JSONBinder{}
+	var userReq requestSignup
+	err = binder.Bind(&userReq, w, r.Body, &bind.Options{
+		MaxBytes:              1 << 20,
+		DisallowUnknownFields: true,
+	})
+	//conver error to http StatusCode
+	status := bindingErrorToHTTPStatus(err)
+	switch status {
+	case http.StatusOK: //if ok continue
+	case http.StatusInternalServerError: //if internal server error log it
+		log.Printf("unknown error while binding requestSignup: %s", err)
+		writeErrorAsJSON(w, status, errors.New("unknown error while parsing request body"))
+		return
+	default: //other errors are printed to user
+		writeErrorAsJSON(w, status, err)
+		return
 	}
 	id, err := srv.app.CreateUser(r.Context(), &user)
 	if err != nil {
