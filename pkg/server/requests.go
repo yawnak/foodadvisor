0b8d5fe5de1db39fdffd1807410ca5824b9c28f4
asstronom <72160919+asstronom@users.noreply.github.com>
2023-03-30 00:41:20 +0300

    feat: add validation tags to requestSignup

--- pkg/server/requests.go
+++ pkg/server/requests.go
@@ -5,10 +5,10 @@ type requestLogin struct {
 	Password string `json:"password"`
 }
 
-type requestJson struct {
-	Id             int32  `json:"id"`
-	Username       string `json:"username"`
-	Password       string `json:"password"`
-	ExpirationDays int32  `json:"expiration"` //in days
-	Role           string `json:"role"`
+type requestSignup struct {
+	Id             int32   `json:"id"`
+	Username       string  `json:"username" validate:"required,alphanumwithunderscore,usernamemax"`
+	Password       string  `json:"password" validate:"required,alphanumwithunderscore,passwordmax"`
+	ExpirationDays int32   `json:"expiration" validate:"gte=0"` //in days
+	Role           *string `json:"-" validate:"isdefault"`
 }
