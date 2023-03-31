1704c12582ba88a66a9484c2c55c2c9ee73e0f26
asstronom <72160919+asstronom@users.noreply.github.com>
2023-04-01 00:21:08 +0300

    feat: validate tags for requestLogin

--- pkg/server/requests.go
+++ pkg/server/requests.go
@@ -1,8 +1,9 @@
 package server
 
 type requestLogin struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required,alphanumwithunderscore,usernamemax"`
+	Password string `json:"password" validate:"required,alphanumwithunderscore,passwordmax"`
+}
 }
 
 type requestSignup struct {
