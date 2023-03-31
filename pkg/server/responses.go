e5a68090b977f897c5e253aad89b6d21635d4b54
asstronom <72160919+asstronom@users.noreply.github.com>
2023-04-01 00:22:45 +0300

    feat: add responses

---
+++ pkg/server/responses.go
@@ -0,0 +1,19 @@
+package server
+
+type responseSuccess struct {
+	SuccessMessage string `json:"success"`
+	HTTPStatusCode int    `json:"-"`
+}
+
+func (s responseSuccess) Success() string {
+	return s.SuccessMessage
+}
+
+func (s responseSuccess) Status() int {
+	return s.HTTPStatusCode
+}
+
+type responseSignup struct {
+	responseSuccess
+	UserId int32 `json:"id"`
+}
