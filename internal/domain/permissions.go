8eaf7533b26014edca78b817c22414625b3d62cf
asstronom <shliakhov.danylo@gmail.com>
2023-04-07 21:59:54 +0300

    feat: add ToContext FromContext for Role

--- internal/domain/permissions.go
+++ internal/domain/permissions.go
@@ -7,9 +7,23 @@ const (
 	PermEditUserRole Permission = "editUserRole"
 )
 
+type roleCtxKey struct{}
+
 type Role struct {
 	Name        string
 	Permissions map[Permission]struct{}
+}
+
+func (u *Role) ToContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, userCtxKey{}, u)
+}
+
+func RoleFromContext(ctx context.Context) (*Role, bool) {
+	role, ok := ctx.Value(roleCtxKey{}).(*Role)
+	if !ok {
+		return nil, false
+	}
+	return role, true
 }
 
 func NewRole(name string, permissions ...Permission) *Role {
