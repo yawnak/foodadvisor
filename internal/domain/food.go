72829b59eb5dff9aed2894b0e96cc2086d0ef461
asstronom <shliakhov.danylo@gmail.com>
2023-04-11 18:42:15 +0300

    feat: BasicAdvise

--- internal/domain/food.go
+++ internal/domain/food.go
@@ -1,6 +1,8 @@
 package domain
 
-import "context"
+import (
+	"context"
+)
 
 type Food struct {
 	Id   int32
@@ -15,8 +17,10 @@ type FoodRepo interface {
 	DeleteFood(ctx context.Context, id int32) error
 	UpdateFood(ctx context.Context, food *Food) error
 	GetFoodByQuestionary(ctx context.Context, questionary *Questionary) ([]Food, error)
+	GetFoodWithoutLastEaten(ctx context.Context, userid int32, limit uint, offset uint) ([]Food, error)
 }
 
 type FoodUsecase interface {
 	CreateFood(ctx context.Context, food *Food) (int32, error)
+	BasicAdvise(ctx context.Context, userid int32, limit uint, offset uint) ([]Food, error)
 }
