f60907eb52ab8103808f116fd1169afa5b950c95
asstronom <shliakhov.danylo@gmail.com>
2023-04-13 17:58:13 +0300

    feat: requestUpdateUser

--- pkg/server/requests.go
+++ pkg/server/requests.go
@@ -17,6 +17,15 @@ type requestSignup struct {
 	Role           *string `json:"-" validate:"isdefault"`
 }
 
+type requestUpdateUser struct {
+	Id             int32   `json:"-" validate:"isdefault"`
+	Username       string  `json:"username" validate:"required,alphanumwithunderscore,usernamemax"`
+	Password       string  `json:"-" validate:"isdefault"`
+	ExpirationDays int32   `json:"expiration" validate:"gte=0"` //in days
+	Role           *string `json:"-" validate:"isdefault"`
+}
+
+
 type requestCreateFood struct {
 	Id   int32  `json:"id" validate:"isdefault"`
 	Name string `json:"name"`
