4df37305077dcc6801a6d1950149c2edb405238e
asstronom <shliakhov.danylo@gmail.com>
2023-04-15 23:02:48 +0300

    feat: mealid from context to context

--- pkg/server/params.go
+++ pkg/server/params.go
@@ -24,3 +24,32 @@ func userIdFromContext(ctx context.Context) (int32, bool) {
 	return uid, ok
 }
 
+type mealIdCtxKey struct{}
+
+func mealIdToContext(ctx context.Context, mealid int32) context.Context {
+	return context.WithValue(ctx, mealIdCtxKey{}, mealid)
+}
+
+func mealIdFromContext(ctx context.Context) (int32, bool) {
+	mealid, ok := ctx.Value(mealIdCtxKey{}).(int32)
+	if !ok {
+		return 0, ok
+	}
+	return mealid, ok
+}
+
+func mealIdParamToCtx(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		temp := chi.URLParamFromCtx(r.Context(), "mealid")
+		if temp == "" {
+			exception.WriteErrorAsJSON(w, http.StatusBadRequest, errors.New("no mealid in URL params"))
+			return
+		}
+		t, err := strconv.Atoi(temp)
+		if err != nil {
+			exception.WriteErrorAsJSON(w, http.StatusBadGateway, errors.New("mealid must be of type int"))
+		}
+		r = r.WithContext(mealIdToContext(r.Context(), int32(t)))
+		next.ServeHTTP(w, r)
+	})
+}
